54799bdf415c3aa00420ab5cda910d1aba2d99ad
H.R. Shadhin <hrshadhin.i386@gmail.com>
2016-09-14 09:55:40 +0600

    transcript report added

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -80,7 +80,7 @@ class ResultController extends Controller
     $session =$request->input('session');
     $subject = Subject::select('name','code')->where('id',$request->input('subject_id'))->first();
     $date=$Final[0]->created_at;
-     $metaDatas= [
+    $metaDatas= [
       'institute' => $institute->name,
       'department' => $department->name,
       'semester' => $semester,
@@ -104,12 +104,145 @@ class ResultController extends Controller
 
   }
   public function postStudent(Request $request){
-    return $results=Exam::with('subject')
+    //first year code start
+    $l1t1=Exam::with('subject')
     ->where('department_id',$request->input('department_id'))
     ->where('session',$request->input('session'))
     ->where('students_id',$request->input('students_id'))
     ->where('exam',$request->input('exam'))
+    ->where('levelTerm','l1t1')
     ->get();
+    //check if first year first semester request exam data present
+    if(!count($l1t1)){
+      $notification= array('title' => 'Not Found!', 'body' => 'Any subject of first year first semester for '.$request->input('exam').' not present!');
+      return redirect()->back()->with("error",$notification);
+    }
+
+    $L1T1data=(object)$this->dataManupulator($l1t1);
+    $l1t2=Exam::with('subject')
+    ->where('department_id',$request->input('department_id'))
+    ->where('session',$request->input('session'))
+    ->where('students_id',$request->input('students_id'))
+    ->where('exam',$request->input('exam'))
+    ->where('levelTerm','l1t2')
+    ->get();
+    //check if first year second semester request exam data present
+    if(!count($l1t2)){
+      $notification= array('title' => 'Not Found!', 'body' => 'Any subject of first year second semester for '.$request->input('exam').' not present!');
+      return redirect()->back()->with("error",$notification);
+    }
+
+    $L1T2data=(object)$this->dataManupulator($l1t2);
+    $fyp=($L1T1data->totalPoint+$L1T2data->totalPoint);
+    $fyc=$L1T1data->totalCredit+$L1T2data->totalCredit;
+    $fyGPA=$fyp/$fyc;
+    $fyData =[
+      'grade' => $this->yearGrade($fyGPA),
+      'point' => $fyGPA,
+      'credit' => $fyc,
+    ];
+    //First Year data colection code end
+    $l2t1=Exam::with('subject')
+    ->where('department_id',$request->input('department_id'))
+    ->where('session',$request->input('session'))
+    ->where('students_id',$request->input('students_id'))
+    ->where('exam',$request->input('exam'))
+    ->where('levelTerm','l2t1')
+    ->get();
+    //check if second year first semester request exam data present
+    if(!count($l2t1)){
+      $notification= array('title' => 'Not Found!', 'body' => 'Any subject of second year first semester for '.$request->input('exam').' not present!');
+      return redirect()->back()->with("error",$notification);
+    }
+    $L2T1data=(object)$this->dataManupulator($l2t1);
+    $l2t2=Exam::with('subject')
+    ->where('department_id',$request->input('department_id'))
+    ->where('session',$request->input('session'))
+    ->where('students_id',$request->input('students_id'))
+    ->where('exam',$request->input('exam'))
+    ->where('levelTerm','l2t2')
+    ->get();
+    //check if second year second semester request exam data present
+    if(!count($l2t2)){
+      $notification= array('title' => 'Not Found!', 'body' => 'Any subject of second year second semester for '.$request->input('exam').' not present!');
+      return redirect()->back()->with("error",$notification);
+    }
+    $L2T2data=(object)$this->dataManupulator($l2t2);
+    $syp=($L2T1data->totalPoint+$L2T2data->totalPoint);
+    $syc=$L2T1data->totalCredit+$L2T2data->totalCredit;
+    $syGPA=$syp/$syc;
+    $syData =[
+      'grade' => $this->yearGrade($syGPA),
+      'point' => $syGPA,
+      'credit' => $syc,
+    ];
+    //Second Year data colection code end
+    $l3t1=Exam::with('subject')
+    ->where('department_id',$request->input('department_id'))
+    ->where('session',$request->input('session'))
+    ->where('students_id',$request->input('students_id'))
+    ->where('exam',$request->input('exam'))
+    ->where('levelTerm','l3t1')
+    ->get();
+    //check if third year first semester request exam data present
+    if(!count($l3t1)){
+      $notification= array('title' => 'Not Found!', 'body' => 'Any subject of Third year first semester for '.$request->input('exam').' not present!');
+      return redirect()->back()->with("error",$notification);
+    }
+    $L3T1data=(object)$this->dataManupulator($l3t1);
+    $l3t2=Exam::with('subject')
+    ->where('department_id',$request->input('department_id'))
+    ->where('session',$request->input('session'))
+    ->where('students_id',$request->input('students_id'))
+    ->where('exam',$request->input('exam'))
+    ->where('levelTerm','l3t2')
+    ->get();
+    //check if third year second semester request exam data present
+    if(!count($l3t2)){
+      $notification= array('title' => 'Not Found!', 'body' => 'Any subject of third year second semester for '.$request->input('exam').' not present!');
+      return redirect()->back()->with("error",$notification);
+    }
+    $L3T2data=(object)$this->dataManupulator($l3t2);
+    $typ=($L3T1data->totalPoint+$L3T2data->totalPoint);
+    $tyc=$L3T1data->totalCredit+$L3T2data->totalCredit;
+    $tyGPA=$typ/$tyc;
+    $tyData =[
+      'grade' => $this->yearGrade($tyGPA),
+      'point' => $tyGPA,
+      'credit' => $tyc,
+    ];
+    //third Year data colection code end
+    //result Data
+    $fPoint = (($fyp+$syp+$typ)/($fyc+$syc+$tyc));
+    $fGrade= $this->yearGrade($fPoint);
+    $fResult="FAIL";
+    if($fPoint>0.00)
+    {
+      $fResult="PASS";
+    }
+    $result=[
+      'point' => $fPoint,
+      'grade' =>$fGrade,
+      'result' => $fResult,
+    ];
+    $institute = Institute::select('name')->first();
+    $department = Department::select('name')->where('id',$request->input('department_id'))->first();
+    $session =$request->input('session');
+    $student= Student::findOrFail($request->input('students_id'));
+    $metaDatas= [
+      'institute' => $institute->name,
+      'department' => $department->name,
+      'session' => $session,
+      'name'    => $student->firstName.' '.$student->middleName.' '.$student->lastName,
+      'fatherName' => $student->fatherName,
+      'motherName' => $student->motherName,
+      'dob' => $student->dob->format('F j,Y'),
+      'idNo' => $student->idNo,
+      'bncReg' => $student->bncReg,
+      'exam' => $request->input('exam')
+    ];
+    $metaData= (object)$metaDatas;
+    return view('reports.result.transcript',compact('metaData','L1T1data','L1T2data','fyData','L2T1data','L2T2data','syData','L3T1data','L3T2data','tyData','result'));
   }
 
 
@@ -121,6 +254,7 @@ class ResultController extends Controller
       $totalScore=$student->percentage_x_weight+$Final[$i]->percentage_x_weight+$LabandQuiz[$i]->percentage_x_weight;
       $totalWeight=$student->weight+$Final[$i]->weight+$LabandQuiz[$i]->weight;
       $gradePoint=$totalScore/$totalWeight;
+      $grade=$this->gradeCalculator($gradePoint);
       $singleSTD =[
         'idNo' => $student->student->idNo,
         'name' => $student->student->firstName.' '.$student->student->middleName.' '.$student->student->lastName,
@@ -139,7 +273,7 @@ class ResultController extends Controller
         'total_score' => $totalScore,
         'total_weight' =>$totalWeight,
         'grade_point' => $gradePoint,
-        'grade' => $this->gradeCalculator($gradePoint),
+        'grade' => $grade[0],
       ];
       $allData[]=$singleSTD;
       $i++;
@@ -150,18 +284,72 @@ class ResultController extends Controller
 
   private function gradeCalculator($point){
     $grade="F";
-    if($point>=80.00)
+    $gpa="0";
+    if($point>=80.00){
       $grade="A";
-    else if($point>=70.00)
+      $gpa="4";
+    }
+    else if($point>=70.00){
       $grade="B";
-    else if($point>=60.00)
+      $gpa="3";
+    }
+    else if($point>=60.00){
       $grade="C";
-    else if($point>=50.00)
+      $gpa="2";
+    }
+    else if($point>=50.00){
       $grade="D";
-    else
+      $gpa="1";
+    }
+    else{
+      $grade="F";
+      $gpa="0";
+    }
+
+    return [$grade,$gpa];
+  }
+
+  private function dataManupulator($l1t1){
+    $totalCredit=0;
+    $totalPoint=0;
+    foreach ($l1t1 as $marks) {
+      $point = ($marks->percentage_x_weight/$marks->weight);
+      $gGpa = $this->gradeCalculator($point);
+      $subPoint=$gGpa[1]*$marks->subject->credit;
+      $totalCredit +=$marks->subject->credit;
+      $totalPoint +=$subPoint;
+      $singleSub=[
+        'course' => $marks->subject->code.': '.$marks->subject->name,
+        'credit' => $marks->subject->credit,
+        'grade'  => $gGpa[0],
+        'point'  =>$subPoint,
+      ];
+      $allSubject[]=$singleSub;
+    }
+    $semGrade = round(($totalPoint/$totalCredit),2);
+    return ['monthYear'=>$l1t1[0]->created_at->format('F,Y'),'semGrade'=>$semGrade,'totalCredit'=>$totalCredit,'totalPoint'=>$totalPoint,'subjects'=>$allSubject];
+  }
+
+  private function yearGrade($point){
+    $grade="F";
+    if($point>=4.00){
+      $grade="A";
+    }
+    else if($point>=3.00){
+      $grade="B";
+    }
+    else if($point>=2.00){
+      $grade="C";
+    }
+    else if($point>=1.00){
+      $grade="D";
+    }
+    else{
       $grade="F";
 
+    }
     return $grade;
+
   }
 
 }
---
+++ resources/views/reports/result/transcript.blade.php
@@ -0,0 +1,375 @@
+<html>
+<head>
+<style>
+html, body {
+  width: 210mm;
+  height: 297mm;
+  /* to centre page on screen*/
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@media print {
+  html, body {
+    width: 210mm;
+    height: 297mm;
+  }
+}
+table {
+  width: 100%;
+}
+.grid th {
+  height: 50px;
+}
+.grid{
+  border-collapse: collapse;
+  text-align: center;
+}
+.grid th, .grid td {
+  border: 1px solid black;
+}
+.gradeSystem {
+  border: 1px solid black;
+  text-align: justify;
+  padding-left: 5px;
+}
+.tb_info td{
+  text-align: left;
+}
+h2{
+  font-size: 1.8em;
+}
+h3{
+  font-size: 1.5em;
+}
+.pull-right{
+  text-align: right;
+}
+.text-center{
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+  <table>
+
+    <tr>
+      <td style="vertical-align:center;text-align:right;"><img src="assets/images/logo.jpg" /></td>
+      <td  colspan="4" style="text-align:center;vertical-align:center">
+        <h2>{{$metaData->institute}}<h2>
+        </td>
+        <td style="text-align:right;vertical-align:top">
+          Book:..................<br>
+          Serial No:..................<br>
+          Date:..................
+        </td>
+
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="4" style="text-align:center;vertical-align:top">
+          <h3>Transcript of Academic Records</h3>
+        </td>
+        <td style="vertical-align:top">
+          <div class="gradeSystem">
+            A= 4(80.00-100%)<br>
+            B= 3(70.00-79.99%)<br>
+            C= 2(60.00-69.99%)<br>
+            D= 1(50.00-59.99%)<br>
+            F= Fail(<49.99%)<br>
+          </div>
+        </td>
+      </tr>
+    </table>
+    <table class="tb_info">
+      <tr>
+        <td><strong>Name Of Student:</strong></td>
+        <td colspan="2">{{$metaData->name}}</td>
+        <td><strong>Date of birth:</strong></td>
+        <td colspan="2"> {{$metaData->dob}}</td>
+        <td colspan="2"><strong>Id No:</strong> {{$metaData->idNo}}</td>
+      </tr>
+      <tr>
+        <td colspan="2"><strong>BNC Reg.</strong> {{$metaData->bncReg}}</td>
+        <td><strong>Father's name:</strong></td>
+        <td colspan="2">{{$metaData->fatherName}}</td>
+        <td><strong>Mother's name:</strong></td>
+        <td colspan="2"> {{$metaData->motherName}}</td>
+
+      </tr>
+      <tr>
+        <td colspan="4">{{$metaData->department}}</td>
+        <td colspan="2"><strong>Examination:</strong> {{$metaData->exam}}</td>
+        <td colspan="2"><strong>Session:</strong> {{$metaData->session}}</td>
+      </tr>
+    </table>
+    <br>
+    <table class="grid">
+
+      <tr>
+        <th colspan="2">Year & Semester</th>
+        <th style="text-align: center" colspan="4">Course </th>
+        <th style="text-align: center">Credit earned</th>
+        <th style="text-align: center">Letter Grade</th>
+        <th style="text-align: center">Point</th>
+        <th>Semester Grade</th>
+        <th>Year Grade GPA</th>
+      </tr>
+
+      @foreach ($L1T1data->subjects as $key => $subject)
+      @if($key==0)
+      <tr>
+        <td rowspan="{{count($L1T1data->subjects)+1}}" colspan="2"> Year 1<br>Semeser 1
+          <br><br>
+          {{$L1T1data->monthYear}}
+        </td>
+        <td colspan="4">{{$subject['course']}}</td>
+        <td>{{$subject['credit']}}</td>
+        <td>{{$subject['grade']}}</td>
+        <td>{{$subject['point']}}</td>
+        <td rowspan="{{count($L1T1data->subjects)+1}}">{{$L1T1data->semGrade}}</td>
+        <td rowspan="{{count($L1T1data->subjects)+count($L1T2data->subjects)+2}}">{{$fyData['grade']}}</td>
+      </tr>
+      @else
+      <tr>
+      <td colspan="4">{{$subject['course']}}</td>
+      <td>{{$subject['credit']}}</td>
+      <td>{{$subject['grade']}}</td>
+      <td>{{$subject['point']}}</td>
+    </tr>
+      @endif
+
+      @endforeach
+
+      <tr>
+        <td colspan="4"><strong>Total Credit<strong></td>
+        <td><strong>{{$L1T1data->totalCredit}}</strong></td>
+        <td></td>
+        <td><strong>{{$L1T1data->totalPoint}}</strong></td>
+
+      </tr>
+      @foreach ($L1T2data->subjects as $key => $subject)
+      @if($key==0)
+      <tr>
+        <td rowspan="{{count($L1T2data->subjects)+1}}" colspan="2"> Year 1<br>Semeser 2
+          <br><br>
+          {{$L1T2data->monthYear}}
+        </td>
+        <td colspan="4">{{$subject['course']}}</td>
+        <td>{{$subject['credit']}}</td>
+        <td>{{$subject['grade']}}</td>
+        <td>{{$subject['point']}}</td>
+        <td rowspan="{{count($L1T2data->subjects)+1}}">{{$L1T2data->semGrade}}</td>
+
+      </tr>
+      @else
+      <tr>
+      <td colspan="4">{{$subject['course']}}</td>
+      <td>{{$subject['credit']}}</td>
+      <td>{{$subject['grade']}}</td>
+      <td>{{$subject['point']}}</td>
+    </tr>
+      @endif
+
+      @endforeach
+
+      <tr>
+        <td colspan="4"><strong>Total Credit<strong></td>
+        <td><strong>{{$L1T2data->totalCredit}}</strong></td>
+        <td></td>
+        <td><strong>{{$L1T2data->totalPoint}}</strong></td>
+
+      </tr>
+      <!-- SCOND year -->
+      @foreach ($L2T1data->subjects as $key => $subject)
+      @if($key==0)
+      <tr>
+        <td rowspan="{{count($L2T1data->subjects)+1}}" colspan="2"> Year 2<br>Semeser 1
+          <br><br>
+          {{$L2T1data->monthYear}}
+        </td>
+        <td colspan="4">{{$subject['course']}}</td>
+        <td>{{$subject['credit']}}</td>
+        <td>{{$subject['grade']}}</td>
+        <td>{{$subject['point']}}</td>
+        <td rowspan="{{count($L2T1data->subjects)+1}}">{{$L2T1data->semGrade}}</td>
+        <td rowspan="{{count($L2T1data->subjects)+count($L2T2data->subjects)+2}}">{{$syData['grade']}}</td>
+
+      </tr>
+      @else
+      <tr>
+      <td colspan="4">{{$subject['course']}}</td>
+      <td>{{$subject['credit']}}</td>
+      <td>{{$subject['grade']}}</td>
+      <td>{{$subject['point']}}</td>
+    </tr>
+      @endif
+
+      @endforeach
+
+      <tr>
+        <td colspan="4"><strong>Total Credit<strong></td>
+        <td><strong>{{$L2T1data->totalCredit}}</strong></td>
+        <td></td>
+        <td><strong>{{$L2T1data->totalPoint}}</strong></td>
+
+      </tr>
+      @foreach ($L2T2data->subjects as $key => $subject)
+      @if($key==0)
+      <tr>
+        <td rowspan="{{count($L2T2data->subjects)+1}}" colspan="2"> Year 2<br>Semeser 2
+          <br><br>
+          {{$L2T2data->monthYear}}
+        </td>
+        <td colspan="4">{{$subject['course']}}</td>
+        <td>{{$subject['credit']}}</td>
+        <td>{{$subject['grade']}}</td>
+        <td>{{$subject['point']}}</td>
+        <td rowspan="{{count($L2T2data->subjects)+1}}">{{$L2T2data->semGrade}}</td>
+
+      </tr>
+      @else
+      <tr>
+      <td colspan="4">{{$subject['course']}}</td>
+      <td>{{$subject['credit']}}</td>
+      <td>{{$subject['grade']}}</td>
+      <td>{{$subject['point']}}</td>
+    </tr>
+      @endif
+
+      @endforeach
+
+      <tr>
+        <td colspan="4"><strong>Total Credit<strong></td>
+        <td><strong>{{$L2T2data->totalCredit}}</strong></td>
+        <td></td>
+        <td><strong>{{$L2T2data->totalPoint}}</strong></td>
+
+      </tr>
+
+      <!-- Third year -->
+      @foreach ($L3T1data->subjects as $key => $subject)
+      @if($key==0)
+      <tr>
+        <td rowspan="{{count($L3T1data->subjects)+1}}" colspan="2"> Year 3<br>Semeser 1
+          <br><br>
+          {{$L3T1data->monthYear}}
+        </td>
+        <td colspan="4">{{$subject['course']}}</td>
+        <td>{{$subject['credit']}}</td>
+        <td>{{$subject['grade']}}</td>
+        <td>{{$subject['point']}}</td>
+        <td rowspan="{{count($L3T1data->subjects)+1}}">{{$L3T1data->semGrade}}</td>
+        <td rowspan="{{count($L3T1data->subjects)+count($L3T2data->subjects)+2}}">{{$tyData['grade']}}</td>
+
+      </tr>
+      @else
+      <tr>
+      <td colspan="4">{{$subject['course']}}</td>
+      <td>{{$subject['credit']}}</td>
+      <td>{{$subject['grade']}}</td>
+      <td>{{$subject['point']}}</td>
+    </tr>
+      @endif
+
+      @endforeach
+
+      <tr>
+        <td colspan="4"><strong>Total Credit<strong></td>
+        <td><strong>{{$L3T1data->totalCredit}}</strong></td>
+        <td></td>
+        <td><strong>{{$L3T1data->totalPoint}}</strong></td>
+
+      </tr>
+      @foreach ($L3T2data->subjects as $key => $subject)
+      @if($key==0)
+      <tr>
+        <td rowspan="{{count($L3T2data->subjects)+1}}" colspan="2"> Year 3<br>Semeser 2
+          <br><br>
+          {{$L3T2data->monthYear}}
+        </td>
+        <td colspan="4">{{$subject['course']}}</td>
+        <td>{{$subject['credit']}}</td>
+        <td>{{$subject['grade']}}</td>
+        <td>{{$subject['point']}}</td>
+        <td rowspan="{{count($L3T2data->subjects)+1}}">{{$L3T2data->semGrade}}</td>
+
+      </tr>
+      @else
+      <tr>
+      <td colspan="4">{{$subject['course']}}</td>
+      <td>{{$subject['credit']}}</td>
+      <td>{{$subject['grade']}}</td>
+      <td>{{$subject['point']}}</td>
+    </tr>
+      @endif
+
+      @endforeach
+
+      <tr>
+        <td colspan="4"><strong>Total Credit<strong></td>
+        <td><strong>{{$L3T2data->totalCredit}}</strong></td>
+        <td></td>
+        <td><strong>{{$L3T2data->totalPoint}}</strong></td>
+
+      </tr>
+
+    </table>
+    <br>
+    <!--- summerize ---->
+    <table class="grid">
+
+      <tr>
+        <th >Year</th>
+        <th>Credit earned </th>
+        <th>Point </th>
+        <th>GPA</th>
+        <th>Letter Grade</th>
+      </tr>
+      <tr>
+        <td> Year 1</td>
+        <td>{{$fyData['credit']}}</td>
+        <td>{{round($fyData['point'],2)}}</td>
+        <td rowspan="4">{{round($result['point'],2)}}</td>
+        <td rowspan="4">{{$result['grade']}}</td>
+      </tr>
+      <tr>
+        <td> Year 2</td>
+        <td>{{$syData['credit']}}</td>
+        <td>{{round($syData['point'],2)}}</td>
+      </tr>
+      <tr>
+        <td> Year 3</td>
+        <td>{{$tyData['credit']}}</td>
+        <td>{{round($tyData['point'],2)}}</td>
+      </tr>
+      <tr>
+        <td style="text-align:right"> <strong>Total</strong></td>
+        <td>{{$fyData['credit']+$syData['credit']+$tyData['credit']}}</td>
+        <td></td>
+      </tr>
+    </table>
+    <!-- bottom -->
+    <table>
+      <tr>
+        <td colspan="3"><td>
+          <td class="pull-right"><strong>Total Credit earned: {{$fyData['credit']+$syData['credit']+$tyData['credit']}}</td>
+            <td class="text-center"><strong>GPA: {{round($result['point'],2)}}</strong></td>
+          </tr>
+          <td colspan="3"><td>
+            <td></td>
+            <td class="text-center"><strong>Result: {{$result['result']}}</strong></td>
+          </tr>
+        </table>
+    <!-- Office --->
+    <table style="text-align:left">
+      <tr>
+        <th >Prepared by</th>
+        <th>Checked & Verified by<br>Date: </th>
+        <th>Principal </th>
+
+      </tr>
+  </table>
+      </body>
+      </html>

90842feaa65d6febd987b574591e543c1719e3b1
H.R.Shadhin <hrshadhin.i386@gmail.com>
2017-01-29 23:33:06 +0600

    triggers create from web added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -145,6 +145,66 @@ Route::group(['middleware' => 'auth'], function()
   //barcode generate
   Route::get('/barcode','barcodeController@index');
   Route::post('/barcode','barcodeController@generate');
+  //db triggers crate route
+  Route::get('/hrs-triggers-init',function(){
+    //create tiggers for manage book stock
+    //book addd trigger
+    DB::unprepared('
+    CREATE TRIGGER `afterBookAdd` AFTER INSERT ON `books` FOR EACH ROW
+    BEGIN
+    insert into stock_books
+    set
+    books_id = new.id,
+    quantity = new.quantity;
+    END
+    ');
+    //after book delete
+    DB::unprepared('
+    CREATE TRIGGER `afterBookDelete` AFTER DELETE ON `books` FOR EACH ROW
+    BEGIN
+    delete from borrow_books where books_id = old.id;
+    delete from stock_books where books_id = old.id;
+    END
+    ');
+    //afeter book update
+    DB::unprepared('
+    CREATE TRIGGER `afterBookUpdate` AFTER UPDATE ON `books` FOR EACH ROW
+    BEGIN
+    UPDATE stock_books
+    set
+    quantity = new.quantity-(old.quantity-quantity)
+    WHERE books_id=old.id;
+    END
+    ');
+    //after borrow book add
+    DB::unprepared('
+    CREATE TRIGGER `afterBorrowBookAdd` AFTER INSERT ON `borrow_books` FOR EACH ROW
+    BEGIN
+    UPDATE stock_books
+    set quantity = quantity-new.quantity
+    where books_id=new.books_id;
+    END
+    ');
+    //after borrow book delete
+    DB::unprepared("
+    CREATE TRIGGER `afterBorrowBookDelete` AFTER DELETE ON `borrow_books` FOR EACH ROW
+    IF (old.Status='Borrowed') THEN
+    UPDATE stock_books
+    set quantity = quantity+old.quantity
+    WHERE books_id=old.books_id;
+    END IF
+    ");
+    //after borrow book update
+    DB::unprepared("
+    CREATE TRIGGER `afterBorrowBookUpdate` AFTER UPDATE ON `borrow_books` FOR EACH ROW
+    IF (new.Status='Returned') THEN
+    UPDATE stock_books
+    set quantity = quantity+new.quantity
+    WHERE books_id=new.books_id;
+    END IF
+    ");
+    return "Done!....";
+  });
 
 
 });
